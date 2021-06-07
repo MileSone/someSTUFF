8661fb73a0391f2a696bd7a8323ba64fc5c95b58
Alegend <mingyaozhu26@gmail.com>
2021-06-07 13:51:55 +0800

    Update pool_graph.js

--- pool_graph.js
+++ pool_graph.js
@@ -33,7 +33,7 @@ Instructions:
 */
 
 const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
+const {graphqlHTTP} = require('express-graphql');
 const graphqlTools = require('graphql-tools');
 const oracledb = require("oracledb");
 const dbConfig = require('./dbconfig.js');
@@ -81,30 +81,31 @@ type Mutation {
 }`;
 
 async function getAllPoolsHelper() {
-  let sql = 'SELECT * FROM POOL_TYPE_BACKUP';
-  let conn = await oracledb.getConnection();
-  let result = await conn.execute(sql);
-  await conn.close();
-  console.log("convertor");
-  console.log(jsonCoverter(result));
-  return jsonCoverter(result);
+    let sql = 'SELECT * FROM POOL_TYPE_BACKUP';
+    let conn = await oracledb.getConnection();
+    let result = await conn.execute(sql);
+    await conn.close();
+    console.log("convertor");
+    console.log(jsonCoverter(result));
+    return jsonCoverter(result);
 }
 
 async function getOnePoolHelper(id) {
-  let sql = 'SELECT * FROM POOL_TYPE_BACKUP b WHERE b.pool_type_id = :id';
-  let binds = [id];
-  let conn = await oracledb.getConnection();
-  let result = await conn.execute(sql, binds);
-  await conn.close();
-  return result.rows[0];
+    let sql = 'SELECT * FROM POOL_TYPE_BACKUP b WHERE b.pool_type_id = :id';
+    let binds = [id];
+    let conn = await oracledb.getConnection();
+    let result = await conn.execute(sql, binds);
+    await conn.close();
+    return result.rows[0];
 }
 
 async function createPoolHelper(input) {
-  let sql = 'BEGIN SELECT pool_seq.nextval INTO :id FROM dual; END;';
-  let conn = await oracledb.getConnection();
-  let binds = {id: {dir: oracledb.BIND_OUT, type: oracledb.NUMBER }};
-  let result = await conn.execute(sql, binds);
-  const newPool = {id: result.outBinds.id,
+    let sql = 'BEGIN SELECT pool_seq.nextval INTO :id FROM dual; END;';
+    let conn = await oracledb.getConnection();
+    let binds = {id: {dir: oracledb.BIND_OUT, type: oracledb.NUMBER}};
+    let result = await conn.execute(sql, binds);
+    const newPool = {
+        id: result.outBinds.id,
         type: input.type,
         pool_nature: input.pool_nature,
         full_name: input.full_name,
@@ -114,53 +115,54 @@ async function createPoolHelper(input) {
         output_seq: input.output_seq,
         odds_type: input.odds_type,
         is_exotic: input.is_exotic,
-        remark: input.remark};
-  const js = JSON.stringify(newPool);
-  sql = 'INSERT INTO POOL_TYPE_BACKUP VALUES(:b)';
-  binds = [js];
-  result = await conn.execute(sql, binds, {autoCommit: true});
-  await conn.close();
-  return newPool;
+        remark: input.remark
+    };
+    const js = JSON.stringify(newPool);
+    sql = 'INSERT INTO POOL_TYPE_BACKUP VALUES(:b)';
+    binds = [js];
+    result = await conn.execute(sql, binds, {autoCommit: true});
+    await conn.close();
+    return newPool;
 }
 
 async function updatePoolHelper(id, input) {
-  let sql = 'SELECT * FROM POOL_TYPE_BACKUP b WHERE b.pool_type_id = :id';
-  let binds = [id];
-  let conn = await oracledb.getConnection();
-  let result = await conn.execute(sql, binds);
-  let j = JSON.parse(result.rows[0][0]);
-  j.type = input.type;
-  j.pool_nature = input.pool_nature;
-  j.full_name = input.full_name;
-  j.short_name = input.short_name;
-  j.pool_type = input.pool_type;
-  j.is_enable = input.is_enable;
-  j.output_seq = input.output_seq;
-  j.odds_type = input.odds_type;
-  j.is_exotic = input.is_exotic;
-  j.remark = input.remark;
-  const js = JSON.stringify(j);
-  sql = 'DELETE FROM POOL_TYPE_BACKUP b WHERE b.pool_type_id = :id';
-  result = await conn.execute(sql, binds, {autoCommit: false});
-  sql = 'INSERT INTO POOL_TYPE_BACKUP VALUES(:b)';
-  binds = [js];
-  result = await conn.execute(sql, binds, {autoCommit: true});
-  await conn.close();
-  return j;
+    let sql = 'SELECT * FROM POOL_TYPE_BACKUP b WHERE b.pool_type_id = :id';
+    let binds = [id];
+    let conn = await oracledb.getConnection();
+    let result = await conn.execute(sql, binds);
+    let j = JSON.parse(result.rows[0][0]);
+    j.type = input.type;
+    j.pool_nature = input.pool_nature;
+    j.full_name = input.full_name;
+    j.short_name = input.short_name;
+    j.pool_type = input.pool_type;
+    j.is_enable = input.is_enable;
+    j.output_seq = input.output_seq;
+    j.odds_type = input.odds_type;
+    j.is_exotic = input.is_exotic;
+    j.remark = input.remark;
+    const js = JSON.stringify(j);
+    sql = 'DELETE FROM POOL_TYPE_BACKUP b WHERE b.pool_type_id = :id';
+    result = await conn.execute(sql, binds, {autoCommit: false});
+    sql = 'INSERT INTO POOL_TYPE_BACKUP VALUES(:b)';
+    binds = [js];
+    result = await conn.execute(sql, binds, {autoCommit: true});
+    await conn.close();
+    return j;
 }
 
 async function deletePoolHelper(id) {
-  let sql = 'SELECT * FROM POOL_TYPE_BACKUP b WHERE b.pool_type_id = :id';
-  let binds = [id];
-  let conn = await oracledb.getConnection();
-  let result = await conn.execute(sql, binds);
-  if (result.rows.length === 0)
-    return null;
-  let j = JSON.parse(result.rows[0][0]);
-  sql = 'DELETE FROM POOL_TYPE_BACKUP b WHERE b.pool_type_id = :id';
-  result = await conn.execute(sql, binds, {autoCommit: true});
-  await conn.close();
-  return j;
+    let sql = 'SELECT * FROM POOL_TYPE_BACKUP b WHERE b.pool_type_id = :id';
+    let binds = [id];
+    let conn = await oracledb.getConnection();
+    let result = await conn.execute(sql, binds);
+    if (result.rows.length === 0)
+        return null;
+    let j = JSON.parse(result.rows[0][0]);
+    sql = 'DELETE FROM POOL_TYPE_BACKUP b WHERE b.pool_type_id = :id';
+    result = await conn.execute(sql, binds, {autoCommit: true});
+    await conn.close();
+    return j;
 }
 
 const resolvers = {
@@ -188,17 +190,29 @@ const resolvers = {
 };
 
 function jsonCoverter(obj) {
-    var metaData,rows = [];
+    var metaData, rows = [];
     metaData = obj.metaData;
-    rows = obj.rows;
+    rows = obj.rows[0];
 
-    var newResult = [];
 
-    metaData.forEach(function (item, index) {
-        var newObjString= '{'+ metaData[index].name + ':' + rows[index] + '}'
-        let newObj = JSON.parse(newObjString);
-        newResult.push(newObj);
+    rows.forEach(function (row, count) {
+        var emptyObjString = '{';
+        metaData.forEach(function (item, index) {
+            var com = '';
+            if (index !== 0) {
+                com = ','
+            }
+            var newObjString = com + '"' + metaData[index].name + '":' + rows[index];
+            emptyObjString += newObjString;
+            if (index == metaData.length) {
+                emptyObjString += '}';
+            }
+        })
+        console.log(emptyObjString);
+        var newObj = JSON.parse(emptyObjString);
+        newResult.push(newObj)
     })
+
 
     return newResult;
 }
@@ -208,45 +222,45 @@ const schema = graphqlTools.makeExecutableSchema({typeDefs, resolvers});
 
 // Create a DB connection pool
 async function startOracle() {
-  try {
-    await oracledb.createPool(dbConfig);
-    console.log("Connection Pool created");
-  } catch (err) {
-    console.error(err);
-  }
+    try {
+        await oracledb.createPool(dbConfig);
+        console.log("Connection Pool created");
+    } catch (err) {
+        console.error(err);
+    }
 }
 
 // Start the webserver
 async function ws() {
-  app.use('/graphql', graphqlHTTP({
-    graphiql: true,
-    schema
-  }));
+    app.use('/graphql', graphqlHTTP({
+        graphiql: true,
+        schema
+    }));
 
-  app.listen(port, function() {
-    console.log('Listening on http://localhost:' + port + '/graphql');
-  });
+    app.listen(port, function () {
+        console.log('Listening on http://localhost:' + port + '/graphql');
+    });
 }
 
 // Do it
 async function run() {
-  await startOracle();
-  await ws();
+    await startOracle();
+    await ws();
 }
 
 async function closePoolAndExit() {
-  console.log("\nTerminating");
-  try {
-    await oracledb.getPool().close(0);
-    console.log("Pool closed");
-    process.exit(0);
-  } catch(err) {
-    console.error(err.message);
-    process.exit(1);
-  }
+    console.log("\nTerminating");
+    try {
+        await oracledb.getPool().close(0);
+        console.log("Pool closed");
+        process.exit(0);
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
 }
 
 process
-  .once('SIGTERM', closePoolAndExit)
-  .once('SIGINT',  closePoolAndExit);
+    .once('SIGTERM', closePoolAndExit)
+    .once('SIGINT', closePoolAndExit);
 run();
